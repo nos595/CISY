d9076f162516ae0bc6c26d721766ec70c8393cd3
Jon Barton <nos595@gmail.com>
2020-12-30 22:43:25 +0000

    Fixed typos!

--- src/components/content.js
+++ src/components/content.js
@@ -16,7 +16,7 @@ const Content = (props) => {
                 <b>It's that easy?</b><br />
                 If only. This is gonna take some willpower on your end. You got this though, try'n keep yourself distracted and don't be refreshing me every five seconds!<br /><br />
                 <b>Why did you make this?</b><br />
-                I assume for the same reason you're here, I'm trying to quit! I find the best way to cut down is simply to gradually increase the amount of time between cigarettes, but by the time I'm setting an alarm for 10+ hours it gets <i>really</i> demotivating and having an alarm go off when the timer is up makes me think about smoking even if I hadn't been up to then. I wanted a way to remove all the extranious information and just get an answer to the important question <i>when asked</i> until it wasn't important anymore. So, like any good nerd, I overengineered the problem! =]<br /><br />
+                I assume for the same reason you're here, I'm trying to quit! I find the best way to cut down is simply to gradually increase the amount of time between cigarettes, but by the time I'm setting an alarm for 10+ hours it gets <i>really</i> demotivating and having an alarm go off when the timer is up makes me think about smoking even if I hadn't been up to then. I wanted a way to remove all the extraneous information and just get an answer to the important question <i>when asked</i> until it wasn't important anymore. So, like any good nerd, I overengineered the problem! =]<br /><br />
                 <b>Who are you?</b><br />
                 A thoroughly average bloke called Jon who's trying to kick the ciggies. If you're (for some reason) interested, here's my <a href={("http://www.twitter.com/nos595")} target={("new")}>Twitter</a>.<br /><br />
                 <b>This is free?</b><br />
@@ -26,7 +26,7 @@ const Content = (props) => {
                 <b>Cookies?</b><br />
                 Nah, I'm not interested in tracking you or selling you anything. Cookies are for eating.<br />For the site to function, it stores two bits of info on your device - the time of your next cigarette and the length of the wait time - that's it. If you want to get rid of that, just click reset and I'll wipe everything.<br />The code is freely available on <a href={("https://github.com/nos595/CISY")} target={("new")}>GitHub</a> for anyone inclined to dig through it.<br /><br />
                 <b>Can I hire you?</b><br />
-                You absolutely can! I have very little formal training and no formal experience, but if you can see past that, I am very much in the market! I've been sat behind a keyboard since I was six and have experience in JS, PHP, SQL, HTML & CSS. This is my first proper forray into React and I'm enjoying it a lot!<br />Get in touch at jonmatthewbarton(at)gmail(dot)com if you want to know more.<br /><br />...Okay, that last one isn't frequently asked, but a guy can dream! =P
+                You absolutely can! I have very little formal training and no formal experience, but if you can see past that, I am very much in the market! I've been sat behind a keyboard since I was six and have experience in JS, PHP, SQL, HTML & CSS. This is my first proper foray into React and I'm enjoying it a lot!<br />Get in touch at jonmatthewbarton(at)gmail(dot)com if you want to know more.<br /><br />...Okay, that last one isn't frequently asked, but a guy can dream! =P
             </p>;
 
             // Starting with a new user
